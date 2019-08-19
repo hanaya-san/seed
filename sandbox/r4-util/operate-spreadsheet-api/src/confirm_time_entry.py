ab251355be3ce1250ea68a014711b12883357a78
rothr4 <ryokurayar4th@gmail.com>
2019-08-20 01:40:54 +0900

    差分理由を表示させるようにした

--- sandbox/r4-util/operate-spreadsheet-api/src/confirm_time_entry.py
+++ sandbox/r4-util/operate-spreadsheet-api/src/confirm_time_entry.py
@@ -6,6 +6,8 @@ from spreadsheets_util import accessor
 from spreadsheets_util import conf
 
 RANGE_NAME = 'D2:H33'
+ATT_ST_DIFF = '出社_差分'
+ATT_FN_DIFF = '退社_差分'
 CHECK_RESULT = '比較結果'
 REASON = '事由'
 
@@ -20,8 +22,6 @@ def get_target_range(uname=''):
 
 def check_time_coindicence(src_range=[],
                            dst_range=[]):
-    _ATT_ST_DIFF = '出社_差分'
-    _ATT_FN_DIFF = '退社_差分'
 
     src_df = pd.DataFrame(src_range,columns=src_range[0]
                           ).drop(0).fillna('')
@@ -30,32 +30,21 @@ def check_time_coindicence(src_range=[],
                           ).drop(0).fillna('')
 
     view_df = src_df.iloc[:, [0,1,3,4]]
-    view_df[_ATT_ST_DIFF] = np.where(
+    view_df[ATT_ST_DIFF] = np.where(
         src_df.iloc[:, 3] == dst_df.iloc[:, 3],
         '-', dst_df.iloc[:, 3]
     )
-    view_df[_ATT_FN_DIFF] = np.where(
+    view_df[ATT_FN_DIFF] = np.where(
         src_df.iloc[:, 3] == dst_df.iloc[:, 3],
         '-', dst_df.iloc[:, 3]
     )
-    view_df[CHECK_RESULT] = check_matching(
-        view_df[_ATT_ST_DIFF],
-        view_df[_ATT_FN_DIFF]
-    )
-    view_df[REASON] = np.where(view_df[CHECK_RESULT] == 'WARN',
-                               '本社、現場の時刻差分', '')
-    view_df[CHECK_RESULT] = check_blank(
-        view_df,
-    )
-    view_df[REASON] = np.where(view_df[CHECK_RESULT] == 'WARN',
-                               '未記入', view_df[REASON])
 
     return view_df
 
 
-def check_matching(src_srs, dst_srs):
+def check_matching(df, src_key, dst_key):
     _srs = np.where(
-        (src_srs == '-') & (dst_srs == '-'),
+        (df[src_key] == '-') & (df[dst_key] == '-'),
         '', 'WARN'
     )
     return _srs
@@ -90,6 +79,20 @@ def main():
                                            get_target_range(uname=uname))
     result_df = check_time_coindicence(src_range=home_range_lst,
                                        dst_range=site_range_lst)
+
+    result_df[CHECK_RESULT] = check_matching(
+        result_df,
+        ATT_ST_DIFF,
+        ATT_FN_DIFF
+    )
+    result_df[REASON] = np.where(result_df[CHECK_RESULT] == 'WARN',
+                                 '本社、現場の時刻差分', '')
+
+    result_df[CHECK_RESULT] = check_blank(result_df)
+    result_df[REASON] = np.where((result_df[CHECK_RESULT] == 'WARN')
+                                 &(result_df[REASON] == ''),
+                                 '未記入', result_df[REASON])
+
     print('[INF] Result:')
     print(result_df)
 
